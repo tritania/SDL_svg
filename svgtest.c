6825a3ae87aafe723c39d616fe1ee0ed24dd2336
Tritania <erikwilson@magnorum.com>
2015-05-03 01:14:15 -0300

    Repo strip down.

--- svgtest.c
+++ svgtest.c
@@ -1,5 +1,13 @@
 #include "SDL_svg.h"
-#include "SDL.h"
+
+#ifdef _WIN32
+#include <SDL.h>
+#endif
+
+#ifdef __linux__ 
+#include <SDL2/SDL.h>
+#endif
+
 #include <signal.h>
 #include <stdlib.h>
 
@@ -10,11 +18,13 @@ int main(int argc, char *argv[])
 {
 int dx=0, dy=0;
 SDL_Surface *screen;
+SDL_Window* gWin = NULL;
+SDL_Renderer* gRenderer = NULL;
+SDL_Texture* image_one = NULL;
 int done;
 SDL_Event event;
 SDL_Rect full;
 double Scalex, Scaley;
-Uint32 Time;
 int redraw=0;
 int code;
 
@@ -24,27 +34,39 @@ int code;
 		exit(0);
 	}
 
+    printf("asdasdasdsadasda");
+    
 	/* Initialize SDL */
 	if ( SDL_Init(SDL_INIT_VIDEO) < 0 )
 	{
-		fprintf(stderr, "Couldn't initialize SDL: %s\n",SDL_GetError());
+		//fprintf(stderr, "Couldn't initialize SDL: %s\n",SDL_GetError());
 		exit(1);
 	}
-	atexit(SDL_Quit);
+    
+    printf("asdasdasda");
+    
 	/* Set 640x480 32 bpp video mode */
-	if ( (screen=SDL_SetVideoMode(WIDTH,HEIGHT,32,SDL_SWSURFACE)) == NULL )
-	{
-		fprintf(stderr, "Couldn't set %dx%dx32 video mode: %s\n", WIDTH, HEIGHT,
-              SDL_GetError());
-		exit(2);
-	}
-	signal(SIGINT, SIG_DFL);
+    gWin = SDL_CreateWindow( "Hold", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 480, 640, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if( gWin == NULL )
+    {
+        printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+        exit(1);
+    }
+    
+    printf("asdasdasda");
+    gRenderer = SDL_CreateRenderer( gWin, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+    if( gRenderer == NULL )
+    {
+        printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+        exit(1);
+    }
+    printf("sdad");
 	full.x = 0;
 	full.y = 0;
 	full.w = screen->w;
 	full.h = screen->h;
 	SDL_FillRect(screen, &full, SDL_MapRGB(screen->format, 0x80, 0x80, 0x80));
-	SDL_UpdateRect(screen,0,0,0,0);
+
 
 	SDL_svg_context *TestImage = SVG_Load (argv[1]);
 	SVG_SetOffset (TestImage,SVG_Width(TestImage)/2,SVG_Height(TestImage)/2);
@@ -56,57 +78,25 @@ int code;
 	Scaley = screen->h / SVG_Height(TestImage);
 	SVG_SetScale (TestImage, Scalex, Scaley);
 	SVG_RenderToSurface (TestImage,screen->w/2+dx,screen->h/2+dy,screen);
+    
+    image_one = SDL_CreateTextureFromSurface(gRenderer, screen);
+    if (image_one == NULL) 
+    {
+        fprintf(stderr, "CreateTextureFromSurface failed: %s\n", SDL_GetError());
+        exit(1);
+    }
+    //SDL_FreeSurface(screen);
+    //screen = NULL;
 
-	SDL_UpdateRect(screen,0,0,0,0);
-	Time = SDL_GetTicks();
-	/* Wait for a keystroke */
+
+
 	done = 0;
 	while ( !done )
 	{
-		SDL_Delay(10);
-		/* Check for events */
-		while ( SDL_PollEvent(&event) )
-		{
-			switch (event.type)
-			{
-			case SDL_KEYDOWN:
-				code=event.key.keysym.sym;
-#define FACTOR 1.1
-				if(code=='+' || code=='=')
-				{
-					Scalex*=FACTOR;
-					Scaley*=FACTOR;
-					redraw=1;
-				}
-				if(code=='-')
-				{
-					Scalex/=FACTOR;
-					Scaley/=FACTOR;
-					redraw=1;
-				}
-				if(code==0x1b) // escape
-					done = 1;
-				break;
-			case SDL_QUIT:
-				done = 1;
-				break;
-			default:
-				break;
-			}
-		}
-		if(redraw)
-		{
-			full.x = 0;
-			full.y = 0;
-			full.w = screen->w;
-			full.h = screen->h;
-			SDL_FillRect(screen, &full, 0xFFFFFF);
-			SVG_SetScale (TestImage,Scalex,Scaley);
-			SVG_RenderToSurface (TestImage,screen->w/2,screen->h/2,screen);
-			SDL_UpdateRect(screen,0,0,0,0);
-			redraw=0;
-		}
-
+        SDL_SetRenderDrawColor( gRenderer, 0xD1, 0xD1, 0xE0, 0xFF );
+        SDL_RenderClear( gRenderer );
+        SDL_RenderCopy(gRenderer, image_one, NULL, NULL);
+        SDL_RenderPresent( gRenderer );
 	}
 	SVG_Free (TestImage);
 	return 0;
