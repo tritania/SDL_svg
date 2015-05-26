05dc15609f39fc1219f7f6e30d21fa081b3fb645
Erik wilson <erikwilson@magnorum.com>
2015-05-26 20:22:23 +0000

    Remove sdl_image from project

--- SDL_svg.h
+++ SDL_svg.h
@@ -3,13 +3,11 @@
 
 #ifdef _WIN32
 #include <SDL.h>
-#include <SDL_image.h>
 #include <windows.h>
 #endif
 
 #ifdef __linux__ 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 #include <sys/stat.h>
 #endif
 
